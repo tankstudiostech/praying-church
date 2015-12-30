cee57d111900e8fd759fccdfc373c2aa7bfdde8a
Ryan Tankersley <ry.tankersley@gmail.com>
2015-12-29 21:40:00 -0600

    Nearly have authentication working.  It's failing a little bit.

--- app/routes/authentication.js
+++ app/routes/authentication.js
@@ -4,7 +4,6 @@ var config = require('../../config/config');
 var md5 = require('md5');
 
 module.exports = function(router, app) {
-    
     router.post('/authenticate', function(req, res) {
         Member.findOne({
             email: req.body.email,
@@ -12,18 +11,39 @@ module.exports = function(router, app) {
            if(err) throw err;
             
             if(!member) {
-                res.json({success: false, message: 'Authentication failed.  User not found.'});
-            } else if(member.pword != md5(req.body.pword)) {
-                res.json({success: false, message: 'Authentication failed.  Wrong password.'});
+                res.json({err: true, message: 'Authentication failed.  User not found.'});
+            } else if(false) {
+                console.log(member.pword);
+                console.log(req.body.pword);
+                res.json({err: true, message: 'Authentication failed.  Wrong password.'});
             } else {
+                console.log(config.secret);
                 var token = jwt.sign(member, app.get(config.secret), {
-                    expiresInMinutes: 1440
+                    expiresIn: 1440 * 60
                 });
                 
                 res.json({
-                    success: true, message: 'Authentication successful!', token: token
+                    err: false, message: 'Authentication successful!', token: token
                 });
             }
         });
+    });
+    
+    router.use(function(req, res, next) {
+        var token = req.body.token || req.query.token || req.headers['x-access-token'];
+        
+        if(token) {
+            jwt.verify(token, app.get(config.secret), function(err, decoded) {
+                if(err) return res.json({err: true, message: 'Failed to authenticate token.'});
+                else {
+                    req.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            return res.status(403).send({
+                err: true, message: 'No token provided.'
+            })
+        }
     });
 };
